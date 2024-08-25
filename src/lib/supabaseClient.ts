6b95f30105b6db6b291a340c1762a3e33bfd2076
PokeToribio <43357212+PokeToribio@users.noreply.github.com>
2024-08-25 16:07:08 +0200

    security

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,5 @@
-// lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qiasvyigtpxlonlmrais.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYXN2eWlndHB4bG9ubG1yYWlzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MDE0ODY0NCwiZXhwIjoxOTY1NzI0NjQ0fQ.uNH0l1O2EWnpzl5AK5aaAODk1fZVZqSyEzzp5VUrWIE';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
 export const supabase = createClient(supabaseUrl, supabaseKey);
